72c00ba76c4d6d56a3c4548d69659bbb1148198f
Darko Krushjanovski <45966349+darkokrushjanovski@users.noreply.github.com>
2023-08-01 14:25:02 +0200

    initial commit

---
+++ src/test/java/com/application/springboot/crudoperations/CrudoperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.springboot.crudoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudoperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
